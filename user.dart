c92ce65eeb831007435d1fdebdd3bd1fe06d2b28
Kavinkavinraj18 <Kavinkavinraj150@gmail.com>
2025-03-13 10:56:58 +0530

    Add files via upload

---
+++ user.dart
@@ -0,0 +1,11 @@
+class User {
+  final String id;
+  final String fullname;
+  final String email;
+  final String state;
+  final String city;
+  final String locality;
+  final String password;
+
+  User({required this.id, required this.fullname, required this.email, required this.state, required this.city, required this.locality, required this.password});
+}
